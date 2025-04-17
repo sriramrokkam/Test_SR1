30f87143425b47837cca546ffbb8f0122f233bb4
Sriram Rokkam <sriramrokkam@gmail.com>
2025-04-17 18:40:02 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Test_SR1",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies if needed
+)
